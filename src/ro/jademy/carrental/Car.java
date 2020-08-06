45a5f989003433c7b5bbe771355ac38b0ad4f52e
Dascalu Dan <danutz_trl@yahoo.com>
2020-08-06 21:56:13 +0300

    Fisrt commit

--- src/ro/jademy/carrental/Car.java
+++ src/ro/jademy/carrental/Car.java
@@ -26,6 +26,73 @@ public class Car {
     // Q: how can we better represent money value?
     public String basePrice;
 
+
     // Q: do we need a constructor other than the default one?
     // Q: how can we better protect the car data?
+
+
+
+    public Car ( String make, String model, Integer year, String carType, String fuelType, Integer doorNumber, String color, String  transmissionType, String engine, String basePrice){
+        this.make = make;
+        this.model = model;
+        this.year = year;
+        this.carType = carType;
+        this.fuelType = fuelType;
+        this.doorNumber = doorNumber;
+        this.color = color;
+        this.transmissionType = transmissionType;
+        this.engine = engine;
+        this.basePrice = basePrice;
+
+    }
+
+
+
+
+    )
+
+
+    public String getMake() {
+        return make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public Integer getYear() {
+        return year;
+    }
+
+    public String getCarType() {
+        return carType;
+    }
+
+    public String getFuelType() {
+        return fuelType;
+    }
+
+    public Integer getDoorNumber() {
+        return doorNumber;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public String getTransmissionType() {
+        return transmissionType;
+    }
+
+    public String getEngine() {
+        return engine;
+    }
+
+    public String getBasePrice() {
+        return basePrice;
+    }
+
+
+
+
 }
--- src/ro/jademy/carrental/Customer.java
+++ src/ro/jademy/carrental/Customer.java
@@ -1,7 +1,39 @@
 package ro.jademy.carrental;
 
+import java.util.ArrayList;
+
 public class Customer {
 
     public String firstName;
     public String lastName;
+
+    public List<Users> Users  = new ArrayList<>();
+
+
+
+
+
+    public Customer () {
+
+        fistName = "";
+        lastname = "";
+
+    }
+
+    public void getCustomer (String firstName, String lastName){
+
+
+    }
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+
+
 }
+
+
